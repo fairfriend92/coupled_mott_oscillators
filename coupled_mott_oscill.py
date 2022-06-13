cc1b1f6a4910ea6fee4d0edca9842b9cc265524d
fairfriend92 <fairfriend92@gmail.com>
2022-06-13 17:22:31 +0200

    .

--- coupled_mott_oscill.py
+++ coupled_mott_oscill.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 # Constants
 Rs      = 1.00  # Sample resistance
 R0      = 1.00  # Load resistance
-C0      = 0.50  # Coupling capacitance
-Cp      = 0.50  # Sample capacitance
+C0      = 0.10  # Coupling capacitance
+Cp      = 0.45  # Sample capacitance
 Vl_th   = 0.50  # Left sample V threshold
-Vr_th   = 0.45  # Right sample V threshold
+Vr_th   = 0.50  # Right sample V threshold
 dV      = 0.01  # Prob. distr. width
-dt      = 0.0001  # Time-step
-sim_len = 20000  # Durantion of sim
+dt      = .001  # Time-step
+sim_len = 2000  # Durantion of sim
 
 # Starting values
 Vcl_0   = 0.0   # Initial left sample capacitor voltage
@@ -27,9 +27,11 @@ tls = 0.0   # Time since last spike of left device
 trs = 0.0   # Time since last spike of right device
 
 # Pre-factors
-a   = R0*(3*Rs*Cp + dt)/(3*R0*Rs*Cp + dt*(R0 - Rs))
-b   = Rs*Cp/(2*R0*(3*Rs*Cp + dt))
-c   = Rs*Cp/(3*Rs*Cp + dt)
+a   = R0*(3.*Rs*Cp + dt)/(3.*R0*Rs*Cp + dt*(R0 - Rs))
+b   = Rs*Cp/(2.*R0*(3.*Rs*Cp + dt))
+c   = Rs*Cp/(3.*Rs*Cp + dt)
+d   = Rs*Cp/(Rs*Cp + dt)
+e   = 2.*R0*C0*Cp/(dt*(2.*R0*C0 - dt))
 
 # Lists
 Il  = []    # Left branch current
@@ -40,22 +42,38 @@ Vcr = []    # Right sample capacitor voltage
 V0  = []    # Coupling capacitor voltage
 
 def getIl_new(I_new, Vcl_old, Vcr_old, V0_old):
-    return a*(I_new/2. - b*(I_new*dt/Cp - Vcl_old + Vcr_old + 2*V0_old))
+    return a*(I_new/2. - b*(I_new*dt/Cp - Vcl_old + Vcr_old + 2.*V0_old))
     
 def getIr_new(I_new, Vcl_old, Vcr_old, V0_old):
-    return a*(I_new/2. + b*(-I_new*dt/Cp - Vcl_old + Vcr_old + 2*V0_old))
+    return a*(I_new/2. + b*(-I_new*dt/Cp - Vcl_old + Vcr_old + 2.*V0_old))
     
-def getV0_new(I_new, Il_new, Ir_new, Vcl_old, Vcr_old, V0_old): 
-    return 0 if C0 == 0 else c*(dt/Cp*(Ir_new - Il_new) + Vcr_old - Vcl_old + 2*V0_old)
+def getV0_new(I_new, Il_new, Ir_new, Vcl_old, Vcr_old, V0_old, I0_new=None):
+    if C0 == 0.:
+        return 0.
+    elif I0_new is not None:
+        return V0_old + I0_new*dt/C0
+    else:
+        return c*(dt/Cp*(Ir_new - Il_new) + Vcr_old - Vcl_old + 2.*V0_old)
     
-def getI0_new(V0_new, V0_old):
-    return (V0_new - V0_old)*C0/dt
+def getI0_new(V0_new, V0_old, I_new=None, Vcl_old=None, Vcr_old=None, Vcl_new=None, Vcr_new=None):
+    if Vcl_new is not None:
+        return e*(Vcl_new/d - Vcl_old - I_new/2.*dt/Cp + V0_old*dt/(2.*R0*Cp)) 
+    elif Vcr_new is not None:
+        return -e*(Vcr_new/d - Vcr_old - I_new/2.*dt/Cp - V0_old*dt/(2.*R0*Cp))
+    else:
+        return (V0_new - V0_old)*C0/dt
     
-def getVcl_new(Il_new, I0_new, Vcl_old):
-    return Vcl_0 if Cp == 0 else Rs*Cp/(Rs*Cp + dt)*(Vcl_old + (Il_new + I0_new)*dt/Cp)
+def getVcl_new(Il_new, I0_new, Vcl_old, Vcr_new=None, V0_new=None):
+    if Vcr_new is not None:
+        return Vcr_new - V0_new
+    else:
+        return Vcl_0 if Cp == 0 else d*(Vcl_old + (Il_new + I0_new)*dt/Cp)
     
-def getVcr_new(Ir_new, I0_new, Vcr_old):
-    return Vcr_0 if Cp == 0 else Rs*Cp/(Rs*Cp + dt)*(Vcr_old + (Ir_new - I0_new)*dt/Cp)
+def getVcr_new(Ir_new, I0_new, Vcr_old, Vcl_new=None, V0_new=None):
+    if Vcl_new is not None:
+        return Vcl_new + V0_new
+    else:
+        return Vcr_0 if Cp == 0 else d*(Vcr_old + (Ir_new - I0_new)*dt/Cp)
     
 def getP(V, ts, V_th, dV):
     if (V < V_th):
@@ -77,31 +95,32 @@ for I_new in I:
         Vcl.append(Vcl_0)
         Vcr.append(Vcr_0)
         V0.append(V0_0)
-    
-    if tls == 0.:
-        solveCircuit(Vcl_0, Vcr[-1])
-    elif trs == 0.:
-        solveCircuit(Vcl[-1], Vcr_0)
-    else:
-        solveCircuit(Vcl[-1], Vcr[-1])
+
+    solveCircuit(Vcl[-1], Vcr[-1])
             
     rand = r.random()    
     if rand < getP(Vcl[-1], tls, Vl_th, dV):        
         tls = 0. 
+        Vcl[-1] = Vcl_0
+        I0[-1] = getI0_new(None, V0[-2], I_new, Vcl[-2], None, Vcl[-1], None)
+        V0[-1] = getV0_new(None, None, None, None, None, V0[-2], I0[-1])
+        Ir[-1] = I_new/2. + 1./(2.*R0)*V0[-1]
+        #Vcr[-1] = getVcr_new(None, None, None, Vcl[-1], V0[-1])
+        Vcr[-1] = getVcr_new(Ir[-1], I0[-1], Vcr[-1])
         print('left')
         continue
-    elif tls == 0.:
-        Vcl[-1] = Vcl_0
-        tls = tls + 1.
     else:
         tls = tls + 1.
         
     if (rand < getP(Vcr[-1], trs, Vr_th, dV)):         
         trs = 0.
-        print('right')
-    elif trs == 0.:
         Vcr[-1] = Vcr_0
-        trs = trs + 1.
+        I0[-1] = getI0_new(None, V0[-2], I_new, None, Vcr[-2], None, Vcr[-1])
+        V0[-1] = getV0_new(None, None, None, None, None, V0[-2], I0[-1])
+        Il[-1] = I_new/2. - 1./(2.*R0)*V0[-1]
+        #Vcl[-1] = getVcl_new(None, None, None, Vcr[-1], V0[-1])
+        Vcl[-1] = getVcl_new(Il[-1], I0[-1], Vcl[-1])
+        print('right')
     else:
         trs = trs + 1.
 
