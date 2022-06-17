ee6344090a18ee095659f2b4114b4ed704905c5c
fairfriend92 <fairfriend92@gmail.com>
2022-06-17 17:51:14 +0200

    added histograms

--- coupled_mott_oscill.py
+++ coupled_mott_oscill.py
@@ -3,25 +3,26 @@ import random as r
 import matplotlib.pyplot as plt
 
 # Constants
-mix     = 0.00                  # Mix factor of new and old V
-Rs      = 1.00                  # Sample resistance
-R0      = 1.00                  # Load resistance
-C0      = 10.0                  # Coupling capacitance
-Cp      = 0.50                  # Sample capacitance
-Vl_th   = 0.45                  # Left sample V threshold
-Vr_th   = 0.50                  # Right sample V threshold
-dV      = .005                  # Prob. distr. width
-dt      = .001                  # Time-step
-sim_len = int(2*(1 + mix)/dt)   # Durantion of sim
-tRef    = 5                    # Refractory period of device
+Rs      = 1.00          # Sample resistance
+R0      = 1.00          # Load resistance
+Cp      = 0.50          # Sample capacitance
+Vl_th   = 0.45          # Left sample V threshold
+Vr_th   = 0.50          # Right sample V threshold
+dV      = .005          # Prob. distr. width
+dt      = .01           # Time-step
+sim_len = int(100/dt)   # Durantion of sim
+tRef    = 5             # Refractory period of device
+
+# Parameters
+C0Array  = [0., 0.5, 10.]    # Coupling capacitance
 
 # Starting values
 Vcl_0   = 0.0   # Initial left sample capacitor voltage
 Vcr_0   = 0.0   # Initial right sample capacitor voltage
 
 # Input
-Vapp        = 1.0*np.ones(sim_len)      # Voltage input
-time        = 1*np.arange(0, sim_len)   # Time-steps
+Vapp    = 1.0*np.ones(sim_len)      # Voltage input
+time    = 1*np.arange(0, sim_len)   # Time-steps
 
 # Counters
 dtl = 0.0   # Time since last spike of left device
@@ -41,8 +42,8 @@ I0      = []    # Coupling capacitor current
 Vcl     = []    # Left sample capacitor voltage
 Vcr     = []    # Right sample capacitor voltage
 V0      = []    # Coupling capacitor voltage
-tsl     = []    # Spike timings of left device
-tsr     = []    # Spike timings of right device
+isil    = []    # Inter spike intervals of left device
+isir    = []    # Inter spike intervals of right device
 
 # Compute current of coupling capacitor   
 def getI0():
@@ -62,21 +63,33 @@ def getVcr(t):
     
 # Compute current of left device (capacitor + sample)
 def getIl(t):
-    return (Vapp[t] - Vcl[-1])/R0    
+    return (Vapp[t] - Vcl[-1])/R0 + I0[-1]  
  
 # Compute current of right device (capacitor + sample) 
 def getIr(t):
-    return (Vapp[t] - Vcr[-1])/R0
+    return (Vapp[t] - Vcr[-1])/R0 - I0[-1]
 
 # Compute probability of firing   
 def getP(V, ts, V_th):
     if (V < V_th):
         return 1. - 1./(1. + ts/dt*np.exp((V - V_th)/dV))  
-    else:
+    else: # This helps with overflow errors
         e = np.exp(-(V - V_th)/dV) 
         return 1. - e/(e + ts/dt) 
         
-def makeFig(x, y, color, xLabel, yLabel, name, yLimBottom = None, yLimTop = None):
+# Solve equations of the circuit
+def solveCircuit(t):
+    I0.append(getI0())
+    V0.append(getV0())
+    Vcl.append(getVcl(t))
+    Vcr.append(getVcr(t))
+    Il.append(getIl(t))
+    Ir.append(getIr(t))    
+    Il_out.append(Il[-1] + I0[-1])
+    Ir_out.append(Ir[-1] - I0[-1])
+    
+# Create 2D plot 
+def makeFig(x, y, color, xLabel, yLabel, name, yLimBottom = None, yLimTop = None):    
     plt.figure()
     fig, ax = plt.subplots(figsize=(15, 4))
     ax.plot(x, y, color, marker='s')
@@ -90,60 +103,93 @@ def makeFig(x, y, color, xLabel, yLabel, name, yLimBottom = None, yLimTop = None
     ax.tick_params(axis = 'both', which = 'both', labelsize = 16, length = 4)
     fig.tight_layout()
     plt.savefig('./figures/' + name + '_C0=' + str(C0) + '.pdf') 
+    plt.close()
 
-# Solve equations of the circuit
-def solveCircuit(t):
-    I0.append(getI0())
-    V0.append(getV0())
-    Vcl.append(getVcl(t))
-    Vcr.append(getVcr(t))
-    Il.append(getIl(t))
-    Ir.append(getIr(t))    
-    Il_out.append(Il[-1] + I0[-1])
-    Ir_out.append(Ir[-1] - I0[-1])
-
-# Mix the last and penultimate voltage values    
-def mixSolutions():
-    Vcl[-1] = (Vcl[-1] + mix*Vcl[-2])/(1 + mix)
-    Vcr[-1] = (Vcr[-1] + mix*Vcr[-2])/(1 + mix)
-
-for t in time:     
-    if len(Vcl) == 0:
-        Vcl.append(Vcl_0)
-        Vcr.append(Vcr_0)
-        V0.append(Vcl_0 - Vcr_0)
+# Create histogram    
+def makeHist(dist, width, color, xLabel, yLabel, name, 
+            xLimLeft = None, xLimRight = None, yLimBottom = None, yLimTop = None):    
+    plt.figure()
+    fig, ax = plt.subplots(figsize=(15, 4)) 
+    plt.hist(dist, bins=np.arange(min(dist), max(dist) + width, width), color=color, alpha=0.5)
+    ax.set(xlabel=xLabel, ylabel=yLabel)
+    ax.xaxis.label.set_size(16)
+    ax.yaxis.label.set_size(16)
     
-    solveCircuit(t)
-    
-    # Relaxation of the device to the insulating phase
-    Vcl[-1] = Vcl[-1] if dtl > tRef else Vcl_0
-    Vcr[-1] = Vcr[-1] if dtr > tRef else Vcr_0
-    
-    if mix != 0.:
-        mixSolutions()
-             
-    rand = r.random()    
-    if rand < getP(Vcl[-1], dtl, Vl_th): 
-        tsl.append(t)
-        dtl = 0.        
-        Vcl[-1] = Vcl_0
-    else:
-        dtl = dtl + 1.
+    if yLimBottom is not None:
+        ax.set_ylim(bottom=yLimBottom)
+    if yLimTop is not None:
+        ax.set_ylim(top=yLimTop)
+    if xLimLeft is not None:
+        ax.set_xlim(left=xLimLeft)
+    if xLimRight is not None:
+        ax.set_xlim(right=xLimRight)
         
-    if (rand < getP(Vcr[-1], dtr, Vr_th)):  
-        tsr.append(t)
-        dtr = 0.        
-        Vcr[-1] = Vcr_0
-    else:
-        dtr = dtr + 1.
+    ax.tick_params(axis = 'both', which = 'both', labelsize = 16, length = 4)
+    fig.tight_layout()
+    plt.savefig('./figures/' + name + '_C0=' + str(C0) + '.pdf') 
+    plt.close()
+
+for C0 in C0Array:
+    print('C0=' + str(C0))
+    for t in time:   
+        # Initial conditions
+        if len(Vcl) == 0:
+            Vcl.append(Vcl_0)
+            Vcr.append(Vcr_0)
+            V0.append(Vcl_0 - Vcr_0)
         
-    t = t + 1
-
-makeFig(time, Vcl[:sim_len], 'black', 'Time (arb. units)', 'Voltage (arb. units)', 'Vcl', 0.0, 0.5)
-makeFig(time, Vcr[:sim_len], 'red', 'Time (arb. units)', 'Voltage (arb. units)', 'Vcr', 0.0, 0.5)
-
-makeFig(time, Il_out, 'black', 'Time (arb. units)', 'Ouput current (arb. units)', 'Il_out')
-makeFig(time, Ir_out, 'red', 'Time (arb. units)', 'Output current (arb. units)', 'Ir_out')
+        solveCircuit(t)
+        
+        # Relaxation of the device to the insulating phase
+        Vcl[-1] = Vcl[-1] if dtl > tRef else Vcl_0
+        Vcr[-1] = Vcr[-1] if dtr > tRef else Vcr_0
+                 
+        # Spiking
+        rand = r.random()    
+        if rand < getP(Vcl[-1], dtl, Vl_th): 
+            isil.append(dtl)
+            dtl = 0.        
+            Vcl[-1] = Vcl_0
+        else:
+            dtl = dtl + 1.
+            
+        if (rand < getP(Vcr[-1], dtr, Vr_th)):  
+            isir.append(dtr)
+            dtr = 0.        
+            Vcr[-1] = Vcr_0
+        else:
+            dtr = dtr + 1.
+            
+        t = t + 1
+    
+    '''
+    # Plot voltage
+    makeFig(time, Vcl[:sim_len], 'black', 'Time (arb. units)', 'Voltage (arb. units)', 'Vcl', 0.0, 0.5)
+    makeFig(time, Vcr[:sim_len], 'red', 'Time (arb. units)', 'Voltage (arb. units)', 'Vcr', 0.0, 0.5)
+    
+    # Plot current
+    makeFig(time, Il_out, 'black', 'Time (arb. units)', 'Ouput current (arb. units)', 'Il_out', 0.0)
+    makeFig(time, Ir_out, 'red', 'Time (arb. units)', 'Output current (arb. units)', 'Ir_out', 0.0)
+    '''
+    
+    # Plot ISI histograms
+    makeHist(isil, 100*dt, 'black', 'Inter spike intervals (arb. units)', 'Counts', 'ISI_l', 0.0, 150)
+    makeHist(isir, 100*dt, 'red', 'Inter spike intervals (arb. units)', 'Counts', 'ISI_r', 0.0, 150)
+    
+    # Reset 
+    dtl = 0.0   
+    dtr = 0.0     
+    I       = []    
+    Il      = []   
+    Ir      = []    
+    Il_out  = []    
+    Ir_out  = []    
+    I0      = []    
+    Vcl     = []    
+    Vcr     = []    
+    V0      = []    
+    isil    = []    
+    isir    = []  
     
     
     
